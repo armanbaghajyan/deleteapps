724af499615d5fbb039fd10c860e3142bc9c7cd3
abaghajyan-vineti <arman.baghajyan@vineti.com>
2019-02-26 16:06:04 +0400

    catching exceptions

--- okta_app_action.py
+++ okta_app_action.py
@@ -3,7 +3,7 @@ from okta_app_info import List_Okta_Apps
 action = int(input("""
 Please choose action.
    1. List duplicatate 
-   2. Delet duplicatte applications  
+   2. Delet duplicate applications  
    3. Delete application with keyword
    4. Delete single application by name
 Your Input: """))
@@ -28,29 +28,20 @@ def del_by_pattern(pattern):
 def del_single_application(label):
     List_Okta_Apps.del_by_app_label(label)
 
-if action not in [1,2,3,4]:
-    print("Wrong Choice You need to input 1, 2, 3 or 4")
-elif (action == 1):
-    list_duplicate()
-elif (action == 2):
-    del_duplicate()
-elif (action == 3):
-    pattern = input("Input Delete pattern: ")
-    del_by_pattern(pattern)
-elif (action == 4):
-    label = input("Input application name to delete: ")
-    del_single_application(label)
-
-# applications_dict = List_Okta_Apps.apps_to_deactivate()
-
-# # for item in applications_dict:
-# #    List_Okta_Apps.deactivate(item)
-
-# # app_list = List_Okta_Apps.delete_inactive()
-
-# for item in applications_dict:
-#     if "cicd" in item['label']:
-#         print(item)
-
-# if action == "1":
-#         list_duplicate()
+try:
+        if action not in [1,2,3,4]:
+                print("Wrong Choice You need to input 1, 2, 3 or 4")
+        elif (action == 1):
+                list_duplicate()
+        elif (action == 2):
+                del_duplicate()
+        elif (action == 3):
+                pattern = input("Input Delete pattern: ")
+                del_by_pattern(pattern)
+        elif (action == 4):
+                label = input("Input application name to delete: ")
+                del_single_application(label)
+except TypeError:
+        print("Nothing to delete")
+except Exception as err:
+        print(err)
